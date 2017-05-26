6e1642e5554a5ee91062097468906c51e28b427d
luongquocan196 <lqa.ntk@gmail.com>
2017-05-26 15:17:11 +0700

    added json

--- landmark_gallery/gej.py
+++ landmark_gallery/gej.py
@@ -1,14 +1,14 @@
 import json
 from os import listdir
-from os import isfile
-from os import join
+from os.path import isfile
+from os.path import join
 
 for adir in listdir('.'):
     data = list()
     if isfile(adir):
         continue
-    with open(adir+'.json', 'w') as outfile:
-        for im in listdir(join('.',adir))[:5]:
+    with open('../landmark_gallery_info/' + adir+'.json', 'w') as outfile:
+        for im in listdir(join('.',adir)):
             dim = dict()
             durl = dict()
             durl['medium']='https://dotrongle.github.io/landmark_gallery/'+adir+'/'+im
