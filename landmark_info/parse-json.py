2166653ff1b3c0874f28fe04e8672682fdf211ac
luongquocan196 <lqa.ntk@gmail.com>
2017-05-27 02:23:15 +0700

    update 27 landmark

--- landmark_info/parse-json.py
+++ landmark_info/parse-json.py
@@ -2,13 +2,12 @@ import json
 
 data = {'name':'', 'service':[], 'rating':0}
 
-data['name'] = 'Khách sạn Rex'
-data['service'].append({'id':1, 'type':"Thông tin chi tiết", 'content':'http://www.rexhotelvietnam.com/', 'icon_id':1})
-data['service'].append({'id':1, 'type':"Đặt bàn", 'content':'http://www.rexhotelvietnam.com/dining/', 'icon_id':3})
-data['service'].append({'id':1, 'type':"Đặt phòng", 'content':'https://www.book-secure.com/index.php?s=results&property=vnhoc18828&arrival=2017-06-11&departure=2017-06-12&accommodation=Premium&code=PROMO-EARLY-BIRD-PROMOTION&adults1=1&children1=0&rooms=1&locale=en_GB&currency=USD&stid=1pdh7wuif&Hotelnames=ASIAVNHTLRex&hname=ASIAVNHTLRex&FirstRoomName=Premium&room=Premium&FirstDate=170611%3B1%3B1%3B0&FSTBKNGCode=PROMO-EARLY-BIRD-PROMOTION&accessCode=PROMO-EARLY-BIRD-PROMOTION&style=DIRECT&HTTP_REFERER=http%3A%2F%2Fwww.rexhotelvietnam.com%2Fdining%2F&profil=SESSION%3D57545826996&__utma=1.1453430872.1495820841.1495820841.1495820841.1&__utmb=1.6.10.1495820841&__utmc=1&__utmx=-&__utmz=1.1495820841.1.1.utmcsr%3Dgoogle%7Cutmccn%3D%28organic%29%7Cutmcmd%3Dorganic%7Cutmctr%3D%28not%20provided%29&__utmv=-&__utmk=200602286&redir=BIZ&rt=1495821237&_code=GoogleAnalytics&nbNightsValue=1', 'icon_id':10})
-data['service'].append({'id':1, 'type':"Khuyến mãi", 'content':'http://www.rexhotelvietnam.com/special-offers/', 'icon_id':6})
-data['rating'] = 4.4
-with open('khach_san_rex.json', 'w') as fp:
+data['name'] = 'Lotte cinema Nowzone'
+data['service'].append({'id':1, 'type':"Thông tin chi tiết", 'content':'https://lottecinemavn.com/vi-vn/rap-phim/ho-chi-minh/nowzone.aspx', 'icon_id':1})
+data['service'].append({'id':1, 'type':"Đặt vé", 'content':'https://lottecinemavn.com/vi-vn/buoc-1.aspx', 'icon_id':2})
+data['service'].append({'id':1, 'type':"Khuyến mãi", 'content':'https://lottecinemavn.com/vi-vn/su-kien/all.aspx', 'icon_id':6})
+data['rating'] = 4.2
+with open('lotte_cinema_nowzone.json', 'w') as fp:
     json.dump(data, fp, ensure_ascii=False)
 
 '''
