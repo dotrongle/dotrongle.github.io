c3f8d4335d9568639caed22d54ff8c703f41fc67
Minh Bui <minhbui1996@outlook.com>
2018-03-13 01:11:10 +0700

    replace with Engl

--- landmark_info/parse-json.py
+++ landmark_info/parse-json.py
@@ -5,9 +5,11 @@ data = {'name':'', 'service':[], 'rating':0}
 data['name'] = 'Museum of Cham Sculpture'
 data['service'].append({'id':1, 'type':"Detailed Information", 'content':'http://chammuseum.vn/en', 'icon_id':1})
 data['service'].append({'id':1, 'type':"Events", 'content':'http://chammuseum.vn/en/news-events/?type=event', 'icon_id':5})
-#data['service'].append({'id':1, 'type':"Khuyến mãi", 'content':'http://kimolong.com/product-category/combo/', 'icon_id':6})
+data['service'].append({'id':1, 'type':"Tour Registration", 'content':'http://www.baotangchungtichchientranh.vn/visit/registration.html', 'icon_id':1})
 data['rating'] = 4.5
 
 
-with open('kim_o_long.json', 'w') as fp:
+with open('banahills.json', 'w') as fp:
     json.dump(data, fp, ensure_ascii=False)
+
+# {"icon_id": 4, "type": "Tour Registration", "content": "http://www.baotangchungtichchientranh.vn/visit/registration.html", "id": 1}]}
