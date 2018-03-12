2c1261d901cbd471ea85cf042c122010b93a821f
Minh Bui <minhbui1996@outlook.com>
2018-03-13 01:45:38 +0700

    replace with Engl

--- landmark_info/parse-json.py
+++ landmark_info/parse-json.py
@@ -2,14 +2,14 @@ import json
 
 data = {'name':'', 'service':[], 'rating':0}
 
-data['name'] = 'Museum of Cham Sculpture'
-data['service'].append({'id':1, 'type':"Detailed Information", 'content':'http://chammuseum.vn/en', 'icon_id':1})
-data['service'].append({'id':1, 'type':"Events", 'content':'http://chammuseum.vn/en/news-events/?type=event', 'icon_id':5})
-data['service'].append({'id':1, 'type':"Tour Registration", 'content':'http://www.baotangchungtichchientranh.vn/visit/registration.html', 'icon_id':1})
+data['name'] = 'Da Nang Cathedral'
+data['service'].append({'id':1, 'type':"Detailed Information", 'content':'http://www.giaoxuchinhtoadanang.org', 'icon_id':1})
+data['service'].append({'id':1, 'type':"Events", 'content':'http://www.giaoxuchinhtoadanang.org/tin-tuc/giao-xu/', 'icon_id':5})
+#data['service'].append({'id':1, 'type':"Tour Registration", 'content':'http://baotangdanang.vn/portal/page/portal/baotang/huong_dan', 'icon_id':1})
 data['rating'] = 4.5
 
 
-with open('banahills.json', 'w') as fp:
+with open('CathedralDanang.json', 'w') as fp:
     json.dump(data, fp, ensure_ascii=False)
 
 # {"icon_id": 4, "type": "Tour Registration", "content": "http://www.baotangchungtichchientranh.vn/visit/registration.html", "id": 1}]}
