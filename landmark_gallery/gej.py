2334656d9ba89db06ec5976fdfcdd4b9afc677d7
Minh Bui <minhbui1996@outlook.com>
2018-03-13 01:40:43 +0700

    replace with Engl

--- landmark_gallery/gej.py
+++ landmark_gallery/gej.py
@@ -2,11 +2,12 @@ import json
 from os import listdir
 from os.path import isfile
 from os.path import join
-
-for adir in listdir('.'):
-    data = list()
+import os
+for adir in ['bana']:
+    data = list()   
     if isfile(adir):
         continue
+    os.mkdir('../landmark_gallery_info/' + adir)
     with open('../landmark_gallery_info/' + adir+'.json', 'w') as outfile:
         for im in listdir(join('.',adir)):
             dim = dict()
@@ -14,5 +15,8 @@ for adir in listdir('.'):
             durl['medium']='https://dotrongle.github.io/landmark_gallery/'+adir+'/'+im
             dim['url']=durl
             data.append(dim)
+
+
+
         json.dump(data, outfile)
 
